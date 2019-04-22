b9938f239a7341d9178b68e51dc962fca4b5f631
Abhi Ravi <abhi@abhicomputer.nomads.utk.edu>
2019-04-22 11:14:09 -0400

    Fixed some bugs

--- FindMyRackAPL/ViewController.swift
+++ FindMyRackAPL/ViewController.swift
@@ -14,14 +14,13 @@ class ViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
-    let regionInMeters: Double = 10000
+    let regionInMeters: Double = 400
     
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationServices()
         createAnnotations(locations: annotationLocations)
         findClosestRack(locations: annotationLocations)
-        print("Screw u")
     }
     
     
@@ -71,28 +70,102 @@ class ViewController: UIViewController, MKMapViewDelegate {
             break
         case .authorizedAlways:
             break
+        @unknown default:
+           print("oops")
         }
     }
     
     //marker data
     let annotationLocations = [
-        ["title": "Will Skelton Greenway", "rackType":"hoop", "address": "2225 Estelle Circle", "latitude":35.961614, "longitude": -83.882901],
+//        [" title": "Will Skelton Greenway", "rackType":"hoop", "address": "2225 Estelle Circle", "latitude":35.961614, "longitude": -83.882901],
+//
+//        ["title": "Ned McWheter Park", "rackType":"hoop", "address": "1648 Riverside", "latitude":35.963440, "longitude": -83.896666],
+//
+//        ["title": "Burlington Branch Library", "rackType":"hoop", "address": "4614 Asheville Hwy", "latitude":36.004390, "longitude": -83.859357],
+//
+//        ["title": "Cansler YMCA", "rackType":"hoop", "address": "616 Jessamine", "latitude":35.978893, "longitude": -83.912293],
+//
+//        ["title": "Farragut High School", "rackType":"hoop", "address": "11237 Kingston Pike, Knoxville, TN 37934", "latitude": 35.887757, "longitude": -84.159864],
+//
+//       ["title": "Farragut Presbyterian Church", "rackType":"hoop", "address": "209 Jamestowne Blvd, Knoxville, TN 37934", "latitude": 35.882292, "longitude": -84.166076],
+//
+//       ["title": "Farragut Town Hall", "rackType":"hoop", "address": "11408 Municipal Center Dr, Farragut, TN 37934", "latitude": 35.878445, "longitude": -84.160917],
+//
+//      ["title": "Campbell Station Park", "rackType":"hoop", "address": "405 N Campbell Station Rd, Farragut, TN 37934", "latitude": 35.888581, "longitude": -84.16714],
+//
+//      ["title": "Farragut Parks", "rackType":"hoop", "address": "301 Watt Rd, Farragut, TN 37934", "latitude": 35.865147, "longitude": -84.225743],
+        ["title": "Will Skelton Greenway", "rackType":"Hoop", "address": "2225 Estelle Circle", "latitude": 35.9616, "longitude": -83.8827],
         
-        ["title": "Ned McWheter Park", "rackType":"hoop", "address": "1648 Riverside", "latitude":35.963440, "longitude": -83.896666],
+        ["title": "Ned McWheter Park", "rackType":"Hoop", "address": "1648 Riverside", "latitude": 35.9635, "longitude": -83.4897],
         
-        ["title": "Burlington Branch Library", "rackType":"hoop", "address": "4614 Asheville Hwy", "latitude":36.004390, "longitude": -83.859357],
+        ["title": "Burlington Branch Library", "rackType":"Hoop", "address": "4614 Asheville Hwy, Knoxville, TN 37914", "latitude": 36.0044, "longitude": -83.8595],
         
-        ["title": "Cansler YMCA", "rackType":"hoop", "address": "616 Jessamine", "latitude":35.978893, "longitude": -83.912293],
+        ["title": "Cansler YMCA", "rackType":"Hitch", "address": "616 Jessamine", "latitude": 35.9789, "longitude": -83.9123],
         
-        ["title": "Farragut High School", "rackType":"hoop", "address": "11237 Kingston Pike, Knoxville, TN 37934", "latitude": 35.887757, "longitude": -84.159864],
+        ["title": "Farragut High School", "rackType":"Hoop", "address": " 11237 Kingston Pike, Knoxville, TN 37934", "latitude": 35.8878, "longitude": -84.1599],
         
-       ["title": "Farragut Presbyterian Church", "rackType":"hoop", "address": "209 Jamestowne Blvd, Knoxville, TN 37934", "latitude": 35.882292, "longitude": -84.166076],
-       
-       ["title": "Farragut Town Hall", "rackType":"hoop", "address": "11408 Municipal Center Dr, Farragut, TN 37934", "latitude": 35.878445, "longitude": -84.160917],
-       
-      ["title": "Campbell Station Park", "rackType":"hoop", "address": "405 N Campbell Station Rd, Farragut, TN 37934", "latitude": 35.888581, "longitude": -84.16714],
-      
-      ["title": "Farragut Parks", "rackType":"hoop", "address": "301 Watt Rd, Farragut, TN 37934", "latitude": 35.865147, "longitude": -84.225743],
+        ["title": "Farragut Presbyterian Church", "rackType":"Hoop", "address": "209 Jamestowne Blvd, Knoxville, TN 37934", "latitude": 35.8823, "longitude": -84.1661],
+        
+        ["title": "Farragut Town Hall", "rackType":"Hitch", "address": "11408 Municipal Center Dr, Farragut, TN 37934", "latitude": 35.8784, "longitude": -84.1609],
+        
+        ["title": "Campbell Station Park across from Herron Rd", "rackType":"Hoop", "address": " 405 N Campbell Station Rd, Farragut, TN 37934", "latitude": 35.8886, "longitude": -84.1671],
+        
+        ["title": "Farragut Parks", "rackType":"Hoop", "address": "301 Watt Rd, Farragut, TN 37934", "latitude": 35.8651, "longitude": -84.2257],
+        
+        ["title": "Turkey Creek Medical Center", "rackType":"Hoop", "address": "10810 Parkside Dr", "latitude": 35.8987, "longitude": -84.1444],
+        
+        ["title": "Olive Garden", "rackType":"Hoop", "address": "10923 Parkside Dr, Knoxville, TN 37934", "latitude": 35.9037, "longitude": -84.148],
+        
+        ["title": "Travelers Call Center", "rackType":"Hoop", "address": "1101 Corridor Park Blvd, Knoxville, TN 37932", "latitude": 35.9201, "longitude": -84.1439],
+        
+        ["title": "Parkwest Hospital", "rackType":"Hoop", "address": "9352 Park W Blvd, Knoxville, TN 37923", "latitude": 35.9178, "longitude": -84.0998],
+        
+        ["title": "Starbucks - Kingston Pike - Bearden Kroger", "rackType":"Hoop", "address": "none", "latitude": 35.9407, "longitude": -83.9854],
+        
+        ["title": "TV Unitarian Church", "rackType":"Hoop", "address": "2931 Kingston Pike", "latitude": 35.9523, "longitude": -83.9524],
+        
+        ["title": "Caswell Park", "rackType":"Hoop", "address": "570 Winona St", "latitude": 35.9809, "longitude": -83.9096],
+        
+        ["title": "Panera", "rackType":"Hoop", "address": "4893 N Broadway", "latitude": 36.0282, "longitude": -83.9288],
+        
+        ["title": "Knox County Health Dept - North Entrance ", "rackType":"Hoop", "address": "140 Dameron Ave", "latitude": 35.978, "longitude": -83.9277],
+        
+        ["title": "Western Plaza", "rackType":"Hitch", "address": "none", "latitude": 35.942, "longitude": -83.9791],
+        
+        ["title": "Forest Park Blvd/Third Creek Trailhead", "rackType":"Hoop", "address": "none", "latitude": 35.9427, "longitude": -83.9815],
+        
+        ["title": "Tyson Park", "rackType":"Hoop", "address": "2351 Kingston Pike", "latitude": 35.9546, "longitude": -83.9448],
+        
+        ["title": "Sterchi Lofts", "rackType":"Hitch", "address": "116 S. Gay street", "latitude": 35.9687, "longitude": -83.9198],
+        
+        ["title": "Emporium", "rackType":"Hitch", "address": "100 S. Gay street", "latitude": 35.9691, "longitude": -83.9203],
+        
+        ["title": "Visitors Center/WDVX", "rackType":"Hitch", "address": "301 S. Gay street", "latitude": 35.9672, "longitude": -83.9195],
+        
+        ["title": "Downtown Wine & Spirits", "rackType":"Hitch", "address": "407 S. Gay St.", "latitude": 35.9662, "longitude": -83.919],
+        
+        ["title": "Harrogate's", "rackType":"Hitch", "address": "411 S. Gay Street", "latitude": 35.9661, "longitude": -83.9189],
+        
+        ["title": "Regions - Miller Building", "rackType":"Hitch", "address": "465 S. Gay street", "latitude": 35.9654, "longitude": -83.9186],
+        
+        ["title": " Krutch Park - east end", "rackType":"Hitch", "address": "521 S. Gay St.", "latitude": 35.9649, "longitude": -83.918],
+        
+        ["title": "East Tennessee History Center", "rackType":"Hitch", "address": "601 Gay Street", "latitude": 35.9639, "longitude": -83.9182],
+        
+        ["title": "One Centre Sq", "rackType":"Hitch", "address": "620 Market street", "latitude": 35.9637, "longitude": -83.918],
+        
+        ["title": "Former Regions / AmSouth Building ", "rackType":"Hitch", "address": "505 S Gay St", "latitude": 35.9651, "longitude": -83.9182],
+        
+        ["title": "S Krutch Park", "rackType":"Hitch", "address": "315 Clinch", "latitude": 35.9643, "longitude": -83.9183],
+        
+        ["title": "Urban Outfitters", "rackType":"Hitch", "address": "503 Market St", "latitude": 35.9647, "longitude": -83.9194],
+        
+        ["title": "Market Square", "rackType":"Hitch", "address": "Market Square", "latitude": 35.9651, "longitude": -83.9191],
+        
+        ["title": "YMCA", "rackType":"Hitch", "address": "605 Clinch", "latitude": 35.9632, "longitude": -83.9211],
+        
+        ["title": "YWCA", "rackType":"Hitch", "address": "492 Clinch", "latitude": 35.9636, "longitude": -83.9195],
+        
       
     ]
     
@@ -130,12 +203,15 @@ class ViewController: UIViewController, MKMapViewDelegate {
         var minimumDistance = 99999.9;
         var minIndex = 0;
         
+         if currentLatitude != nil && currentLongitude != nil {
         for object in annotationLocations{
             
             if let actualLongitude = object["longitude"] as? Double, let actualLatitude = object["latitude"] as? Double {
                 //print(i, ": longitude = ", actualLongitude, "   latitude = ", actualLatitude)
                 
-                var distanceLatitude = actualLatitude - currentLatitude!
+                    var distanceLatitude = actualLatitude - currentLatitude!
+                
+                
                 if distanceLatitude < 0{
                     distanceLatitude *= -1
                 }
@@ -168,11 +244,9 @@ class ViewController: UIViewController, MKMapViewDelegate {
         // 1.
         mapView.delegate = self
         
-        /////////
-        let currentLocation: CLLocation!
-        currentLocation =  locationManager.location
-        let  sourceLong =  currentLocation.coordinate.longitude
-        let sourceLat = currentLocation.coordinate.latitude
+            guard let  sourceLong =  currentLongitude else { return print("oops") }
+            guard let sourceLat = currentLatitude else { return print("oops") }
+            
         // 2.
         let sourceLocation = CLLocationCoordinate2D(latitude: sourceLat, longitude: sourceLong)
         
@@ -233,9 +307,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
         }
     }
     
-        
         /********************** ROUTING CODE (WIP) *************************/
-        
+    }
         
 }
 
